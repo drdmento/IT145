12d0e6f1e985c4a9c8c011711b0d4f0999fe5b6a
drdmento <jerdawso@proton.me>
2026-05-30 19:55:38 -0400

    Update Cat.java

--- Cat.java
+++ Cat.java
@@ -1,10 +1,25 @@
+//creates a class called Cat
 public class Cat {
-	int catSpaceNumber;
-	
-	public Cat() {
-		catSpaceNumber = 0;
-	}
-	
-	
+  //creates a private integer variable named catSpaceNumber
+  private int catSpaceNumber;
 
+  //creates a constructor named cat
+ public Cat(){
+	 //initialize variable with a value of 0
+      catSpaceNumber = 0;
+  }
+ 
+ //creates a getter named getCatSpace Number
+ public int getCatSpaceNumber() {
+	 //returns the value of the variable catSpaceNumber
+	return catSpaceNumber;
+ }
+ 
+ // creates a setter that accepts the integer catSpaceNumber
+ public void setCatSpaceNumber(int catSpaceNumber) {
+	 //sets the field to the value of the user input variable of the same name
+	this.catSpaceNumber = catSpaceNumber;
+ }
+
+  
 }
